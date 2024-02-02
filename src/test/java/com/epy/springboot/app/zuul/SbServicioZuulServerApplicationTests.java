c23fdecbd514b6e227aa0a07eace2014b262e99e
abel <parionayauricasa@gmail.com>
2024-02-02 17:26:55 -0500

    cre

---
+++ src/test/java/com/epy/springboot/app/zuul/SbServicioZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epy.springboot.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbServicioZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
